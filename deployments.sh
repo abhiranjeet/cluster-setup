b3d7746ad914cd31f08afe5228f45eae44808d81
abhiranjeet <65591112+abhiranjeet@users.noreply.github.com>
2020-12-03 16:39:32 +0530

    Update deployments.sh

--- deployments.sh
+++ deployments.sh
@@ -7,3 +7,19 @@ export olm_release=0.15.1
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${olm_release}/crds.yaml
 kubectl apply -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/${olm_release}/olm.yaml
 echo "TASK 2....OLM Deployed"
+
+echo "TASK 3....Deploying KubeVirt"
+export VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases | grep tag_name | grep -v -- '-rc' | head -1 | awk -F': ' '{print $2}' | sed 's/,//' | xargs)
+echo $VERSION
+kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml
+kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml
+echo "TASK 3....KubeVirt Deployed"
+
+echo "TASK 4....Deploying Virtctl"
+VERSION=$(kubectl get kubevirt.kubevirt.io/kubevirt -n kubevirt -o=jsonpath="{.status.observedKubeVirtVersion}")
+ARCH=$(uname -s | tr A-Z a-z)-$(uname -m | sed 's/x86_64/amd64/') || windows-amd64.exe
+echo ${ARCH}
+curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-${ARCH}
+chmod +x virtctl
+install virtctl /usr/local/bin
+echo "TASK 4....Virtctl Deployed"
