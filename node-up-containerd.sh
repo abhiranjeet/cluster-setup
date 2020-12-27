5c0d1712c1a9997b8dc8b25352bb60a24e75023e
abhiranjeet <65591112+abhiranjeet@users.noreply.github.com>
2020-12-27 23:17:03 +0530

    Update node-up-containerd.sh

--- node-up-containerd.sh
+++ node-up-containerd.sh
@@ -32,10 +32,7 @@ echo "TASK 3....Done"
 echo "TASK 4....Installing Container-D"
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update && sudo apt-get install -y containerd.io
 sudo mkdir -p /etc/containerd
