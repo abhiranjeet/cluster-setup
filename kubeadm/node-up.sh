dd684297b3370c56ae9734dfedbd137c1893bccc
abhiranjeet <65591112+abhiranjeet@users.noreply.github.com>
2020-12-27 23:21:44 +0530

    Update node-up.sh

--- kubeadm/node-up.sh
+++ kubeadm/node-up.sh
@@ -1,1 +1,50 @@
+apt-get update
 
+echo "TASK 1....Disabling Firewall"
+ufw disable
+echo "TASK 1....Done"
+
+echo "TASK 2....Disabling Swap"
+swapoff -a
+sed -i '/swap/d' /etc/fstab
+echo "TASK 2....Done"
+
+echo "TASK 3....Adding Sysctl Settings"
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+# sudo sysctl --system
+echo "TASK 3....Done"
+
+echo "TASK 4....Installing Container-D"
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update && sudo apt-get install -y containerd.io
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+echo "TASK 4....Done"
+
+echo "TASK 5....Adding Kubernetes Repository"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+echo "TASK 5....Done"
+
+echo "TASK 6....Installing Kubernetes v1.18"
+apt update && apt install -y kubeadm=1.18.5-00 kubelet=1.18.5-00 kubectl=1.18.5-00
+echo "TASK 6....Done
