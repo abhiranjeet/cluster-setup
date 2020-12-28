5f27a2e60105d8d65d2c962af8393340e1475564
abhiranjeet <65591112+abhiranjeet@users.noreply.github.com>
2020-12-28 13:14:29 +0530

    Update node-up-bare-metal.sh

--- node-up-bare-metal.sh
+++ node-up-bare-metal.sh
@@ -23,7 +23,7 @@ sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
 sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 echo "TASK 3....Done"
 
-echo "TASK 4....Installing Docker Engine"
+echo "TASK 4....Installing Docker Engine as default container runtime"
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
