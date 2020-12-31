5f21684f449577758128c7d7860b761c52534616
abhiranjeet <65591112+abhiranjeet@users.noreply.github.com>
2020-12-31 09:55:46 +0530

    Create pod-up-ubuntu.sh

---
+++ podman/pod-up-ubuntu.sh
@@ -0,0 +1,23 @@
+sudo apt install figlet
+figlet "PODMAN - The Hero"
+
+sudo apt-get -y update
+. /etc/os-release
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install podman
+
+echo "Podman has been installed on your Ubuntu Machine"
+
+figlet "Buildah - The Sidekick"
+echo "Installing Buildah"
+. /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${ID^}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${ID^}_${VERSION_ID}/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update -qq
+sudo apt-get -qq -y install buildah
+
+echo "Buildah has been installed on your ubuntu machine"
