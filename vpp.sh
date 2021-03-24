91a207cfbc3315ec1579ac6677345cd4fbf5a7b6
abhiranjeet <65591112+abhiranjeet@users.noreply.github.com>
2021-03-24 16:34:25 +0530

    Update vpp.sh

--- vpp.sh
+++ vpp.sh
@@ -1,8 +1,7 @@
-figlet "Restart VPP"
 sudo vppctl delete interface memif memif1/0
 sudo vppctl delete interface memif memif1/1
 sudo systemctl restart vpp
-ping -c 2 192.168.122.90
+ping -c 2 192.168.122.93
 echo "Creating Interfaces"
 sudo vppctl create memif socket id 1 filename /tmp/memif1.sock
 sudo vppctl create interface memif id 0 socket-id 1 ring-size 1024 buffer-size 2048 master rx-queues 1 tx-queues 1
@@ -20,15 +19,15 @@ sudo vppctl set interface l2 xconnect memif1/1 GigabitEthernet0/5/0
 sudo vppctl set interface l2 xconnect GigabitEthernet0/5/0 memif1/1
 
 echo "VPP in container"
-sudo docker restart ns-client-2
-ping -c 4 192.168.122.90
-sudo docker exec -it ns-client-2 vppctl delete interface memif memif1/0
-sudo docker exec -it ns-client-2 vppctl delete interface memif memif1/1
-sudo docker exec -it ns-client-2 vppctl create memif socket id 1 filename /tmp/memif1.sock
-sudo docker exec -it ns-client-2 vppctl create interface memif id 0 socket-id 1 ring-size 1024 buffer-size 2048 slave rx-queues 1 tx-queues 1
-sudo docker exec -it ns-client-2 vppctl create interface memif id 1 socket-id 1 ring-size 1024 buffer-size 2048 slave rx-queues 1 tx-queues 1
-sudo docker exec -it ns-client-2 vppctl set interface state memif1/0 up
-sudo docker exec -it ns-client-2 vppctl set interface state memif1/1 up
-sudo docker exec -it ns-client-2 vppctl set interface l2 xconnect memif1/0 memif1/1
-sudo docker exec -it ns-client-2 vppctl set interface l2 xconnect memif1/1 memif1/0
+sudo docker restart ns-client-1
+ping -c 4 192.168.122.93
+sudo docker exec -it ns-client-1 vppctl delete interface memif memif1/0
+sudo docker exec -it ns-client-1 vppctl delete interface memif memif1/1
+sudo docker exec -it ns-client-1 vppctl create memif socket id 1 filename /tmp/memif1.sock
+sudo docker exec -it ns-client-1 vppctl create interface memif id 0 socket-id 1 ring-size 1024 buffer-size 2048 slave rx-queues 1 tx-queues 1
+sudo docker exec -it ns-client-1 vppctl create interface memif id 1 socket-id 1 ring-size 1024 buffer-size 2048 slave rx-queues 1 tx-queues 1
+sudo docker exec -it ns-client-1 vppctl set interface state memif1/0 up
+sudo docker exec -it ns-client-1 vppctl set interface state memif1/1 up
+sudo docker exec -it ns-client-1 vppctl set interface l2 xconnect memif1/0 memif1/1
+sudo docker exec -it ns-client-1 vppctl set interface l2 xconnect memif1/1 memif1/0
 figlet "Done"
